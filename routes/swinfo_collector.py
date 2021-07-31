5ce9eaf59dca37c4b6a9ab62f8c4999ea5485b8a
gerawey <gera_ptn@hotmail.com>
2021-07-31 14:07:46 -0700

    Codigo para agregar switch y conexion

---
+++ routes/swinfo_collector.py
@@ -0,0 +1,105 @@
+import bottle
+from modules.bottles import BottleJson
+from modules.swinfo_collector import (
+    add_switch,
+    add_connect
+)
+
+app = BottleJson()
+
+@app.post("/switch")
+def add_switch(*args, **kwargs):
+    payload = bottle.request.query
+    print(payload.dict)
+    try:
+        #switch_id: int(payload['switch_id'])
+        serial_number = str(payload['Serial number'])
+        model = str(payload['Model'])
+        ports = int(payload['ports'])
+        description = str(payload['description'])
+        id = str(payload['id'])
+        respuesta = creador_nota(**payload)
+        raise bottle.HTTPError(201, "Switch Agregado")
+    except:
+        raise bottle.HTTPError(400, "Error no se pudo agregar switch")
+    raise bottle.HTTPError(500,"Error general")
+
+
+@app.get("/switch/<switch_id>")
+def switch_by_id(*args, **kwargs):
+    payload = bottle.request.json
+    print(payload)
+    try:
+        id = str(payload['id'])
+        print("ID valida")
+        respuesta = query_information(**payload)
+        raise bottle.HTTPError(201)
+    except:
+        raise bottle.HTTPError(501, "ID invalida")
+    raise bottle.HTTPError(500, "Error general")
+
+
+@app.get("/switch/all")
+def get_switch_all(*args, **kwargs):
+    try:
+       respuesta = get_switch_all()
+    except:
+        raise bottle.HTTPError(500, "Error interno")
+    raise bottle.HTTPError(200, respuesta)
+
+
+@app.get("/connect/new")
+def add_connect(*args, **kwargs):
+    payload = bottle.request.query
+    print(payload.dict)
+    try:
+        #switch_id: int(payload['switch_id'])
+        switch_in_id = str(payload['switch in id'])
+        switch_out_id = str(payload['switch out id'])
+        port_in = str(payload['port in'])
+        port_out = str(payload['port out'])
+        id = str(payload['id'])
+        respuesta = creador_nota(**payload)
+        raise bottle.HTTPError(201, "Conexion Agregada")
+    except:
+        raise bottle.HTTPError(400, "Error no se pudo agregar conexion")
+    raise bottle.HTTPError(500,"Error general")
+
+@app.get("/connect/<conexion_id>")
+def get_connect_by_id(*args, **kwargs):
+        payload = bottle.request.json
+        print(payload)
+        try:
+            id = str(payload['id'])
+            print("ID valida")
+            respuesta = query_information(**payload)
+            raise bottle.HTTPError(201)
+        except:
+            raise bottle.HTTPError(501, "ID invalida")
+        raise bottle.HTTPError(500, "Error general")
+
+@app.get("/switch/<switch_id>/ports/all")
+def get_switch_all_ports(*args, **kwargs):
+    payload = bottle.request.json
+    print(payload)
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code=501, message="Not implemented")
+
+
+@app.get("/switch/<switch_id>/ports/free")
+def get_switch_free_ports(*args, **kwargs):
+    payload = bottle.request.json
+    print(payload)
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code=501, message="Not implemented")
+
+
+@app.get("/switch/<switch_id>/ports/busy")
+def get_switch_busy_ports(*args, **kwargs):
+    payload = bottle.request.json
+    print(payload)
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code=501, message="Not implemented")
