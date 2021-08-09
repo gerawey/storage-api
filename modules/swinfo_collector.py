9bc73edbb3a042ce3568f16f0160e0a0270ef986
gerawey <gera_ptn@hotmail.com>
2021-08-09 16:42:56 -0700

    Modules update

--- modules/swinfo_collector.py
+++ modules/swinfo_collector.py
@@ -1,53 +1,90 @@
-from os import environ, urandom
-import hashlib
-from bottle import response, request
-import jwt
-import binascii
-from functools import wraps
-import models.auth as mauth
-
-
-
-
-
-
-
 import json
-from modules.storage import *
+from datetime import datetime
+from modules.storage import (
+    store_string,
+    store_bytes,
+    query_storage,
+    get_storage_file
+)
 
-#
-# ...
-#
+def add_switch(switch_id = None, serial_number = None, model = None, ports = None, description = None):
 
-def add_switch(id_switch=None, serial_number=None, model=None, ports=None, description=None):
-    print("Desde modulo add_switch")
-    print(serial_number, model, ports, description)
-    para_almacenar = {"id_switch": id_switch,"serial_number": serial_number, "model": model, "ports":ports, "description":description }
-    nombre_de_archivo = f"{id_switch}-{title}.json"
+    print("Desde Modulo store")
+    print(switch_id, serial_number, model, ports, description)
+    print("Exito")
+
+    almacenable = {
+        "switch_id": switch_id,
+        "serial_number": serial_number,
+        "model": model,
+        "ports": ports,
+        "rdescription": description
+    }
+    nombre_de_archivo = f"{switch_id}-{title}.json"
     datos = store_string(
-        "mi-carpeta3",
+        "/switch",
         nombre_de_archivo,
-        json.dumps(para_almacenar)
+        json.dumps(almacenable)
     )
-    return "Exito"
+    return datos
 
+def get_switch_all(switch=None):
+    query_result = query_storage(
+        "/switch",
+    )
+    if switch is None:
+        return query_result["content"]
 
+def switch_by_id(movie_id=None):
+    query_result = query_storage(
+        "/switch/<switch_id>",
+    )
+    if switch_id is not None:
+        return [
+           r
+           for r in query_result["content"]
+           if movie_id in r
+        ]
+    print("Hecho")
 
+def update_switch(switch_id = None, serial_number = None, model = None, ports = None, description = None):
+    print("Desde Modulo store")
+    print(switch_id, serial_number, model, ports, description)
+    print("Exito")
 
+    almacenable = {
+        "switch_id": switch_id,
+        "serial_number": serial_number,
+        "model": model,
+        "ports": ports,
+        "description": description
+    }
+    nombre_de_archivo = f"{switch_id}.json"
+    datos = store_string(
+        "/switch",
+        nombre_de_archivo,
+        json.dumps(almacenable),
+        update=True
+    )
+    return datos
 
+def add_review(connect_id = None, switch_in_id = None, switch_out_id = None, port_in = None,port_out = None):
 
+    print("Desde Modulo add_review")
+    print(connect_id, switch_in_id, switch_out_id, port_in, port_out)
+    print("Exito")
 
-
-def add_connect(id_connect=None, switch_in_id=None, switch_out_id=None, port_in=None, port_out=None, description=None):
-    print("Desde modulo almacenar_cheat")
-    #print(nombre, eda)
-    para_almacenar = {"id_connect": id_connect,"switch_in_id": switch_in_id, "switch_out_id": switch_out_id, "port_in":port_in, "port_out":port_out, "description":description }
-    json_text = json.dumps(para_almacenar)
-    store_string('mi-carpeta', nombre, para_almacenar)
-    return "Exito"
-
-
-
-
-
-
+    almacenable = {
+        "connect_id": connect_id,
+        "switch_in_id":switch_in_id,
+        "switch_out_id": switch_out_id,
+        "port_in": port_in,
+        "port_out": port_out
+    }
+    nombre_de_archivo = f"{connect_id}.json"
+    datos = store_string(
+        "/connect",
+        nombre_de_archivo,
+        json.dumps(almacenable)
+    )
+    return datos
