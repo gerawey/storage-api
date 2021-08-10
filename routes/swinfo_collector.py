168c1275d654db6f2500a7395f368387ee28b621
gerawey <gera_ptn@hotmail.com>
2021-08-10 15:10:45 -0700

    error arreglao

--- routes/swinfo_collector.py
+++ routes/swinfo_collector.py
@@ -14,11 +14,9 @@ app = BottleJson()
 @app.get("/")
     #Default route
 
- ```` curl http://localhost:8080/switch/addswitch -X POST \
--H 'Content-Type: application/json' -d \
-'{"switch_id": "001","serial_number": "FGH11549", \
- "model": "Catalyst", "ports": "24", \
-"description": "Esta en el IDF 3"}' ````
+
+ # curl http://localhost:8080/switch/addswitch -X POST  -H 'Content-Type: application/json' -d '{"switch_id": "001","serial_number": "FGH11549", "model": "Catalyst", "ports": "24", "description": "Esta en el IDF 3"}'
+
 
 @app.post("/addswitch")
 def addswitch(*args, **kwargs):
